49fac3a10903e196e04c6e9cbbe9959d64bb6722
maicon-soares <maiconsoares1394@gmail.com>
2023-11-21 10:00:58 -0300

    fix bug

--- src/components/CardRepository.js
+++ src/components/CardRepository.js
@@ -1,16 +1,24 @@
-import Link from 'next/link'
-import styles from '../styles/Repository.module.css'
+import Link from "next/link";
+import styles from "../styles/Repository.module.css";
 
-export function CardRepository({repository, deleteRepository}){
-
-    return(
-        <div className={styles.container}>
-            <div className={styles.close} onClick={((e) => deleteRepository(e, repository.id))}/>
-            <Link href={repository.url}>
-                <a target="_blank">
-                    <h1>{repository.name}</h1>
-                </a>
-            </Link>
-        </div>
-    )
+export function CardRepository({ repository, deleteRepository }) {
+  return (
+    <div className={styles.container}>
+      <div
+        className={styles.close}
+        onClick={(e) => deleteRepository(e, repository.id)}
+      />
+      <Link
+        href={
+          repository.url.startsWith("https://")
+            ? repository.url
+            : `https://${repository.url}`
+        }
+      >
+        <a target="_blank">
+          <h1>{repository.name}</h1>
+        </a>
+      </Link>
+    </div>
+  );
 }
